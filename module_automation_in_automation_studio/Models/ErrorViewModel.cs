fb280a2ce297ab81596b9d612e02bd990671d9a1
olivier-training-profile <olivier.delmotte@avanade.com>
2023-07-17 16:30:42 +0200

    Init

---
+++ module_automation_in_automation_studio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace module_automation_in_automation_studio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
